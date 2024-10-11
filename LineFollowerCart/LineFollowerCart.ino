f8462ee0c44a20fbb0d2a123397522ac9afe4ca6
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2024-10-10 23:20:43 -0300

    Optimize Sensor Logic and Enhance Motor Control
    Refactored sensor detection logic with constant threshold variables for improved clarity. Streamlined motor control functions to enhance efficiency and reduce redundancy, facilitating future modifications.

--- LineFollowerCart/LineFollowerCart.ino
+++ LineFollowerCart/LineFollowerCart.ino
@@ -1,68 +1,110 @@
 #include <AFMotor.h>
-AF_DCMotor motorE(1, MOTOR12_64KHZ);
-AF_DCMotor motorD(4, MOTOR12_64KHZ);
 
-const int NUM_SENSORS = 5;
-const int IR_LED_PIN = 2;
-//const int IR_RECEIVER_PINS[NUM_SENSORS] = {A4, A3, A2, A1, A0};
+AF_DCMotor motorE(3, MOTOR12_64KHZ);
+AF_DCMotor motorD(2, MOTOR12_64KHZ);
+
+const int motorSpeed = 110, 
+const int turningMotorSpeed = 170;
+const int lineThresh = 200;
+const int noLineThresh = 20;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(IR_LED_PIN, OUTPUT);
-  digitalWrite(IR_LED_PIN, HIGH);
-
-  motorD.setSpeed(200);
-  motorD.run(RELEASE);
-  motorE.setSpeed(200);
-  motorE.run(RELEASE);
+  motorD.setSpeed(motorSpeed);
+  motorE.setSpeed(motorSpeed);
+  stopMotors(); 
   delay(500);
 }
  
 void loop() {
-  int S1_VALUE = analogRead(A0);
-  int S2_VALUE = analogRead(A1);
-  int S3_VALUE = analogRead(A2);
-  int S4_VALUE = analogRead(A3);
-  int S5_VALUE = analogRead(A4);
- // delay(500);
-  
-  if(S1_VALUE && S2_VALUE && S4_VALUE && S5_VALUE > 200 && S3_VALUE < 100 ){
+  int s1v = analogRead(A0); //Left most sensor
+  int s2v = analogRead(A1); //Left sensor
+  int s3v = analogRead(A2); //Middle sensor
+  int s4v = analogRead(A3); //Right sensor
+  int s5v = analogRead(A4); //Right most sensor
+
+  //If only middle sensor detects black line 1-1-0-1-1
+  if((s1v > lineThresh) && (s2v > lineThresh) && (s3v < noLineThresh) && (s4v > lineThresh) && (s5v > lineThresh)){
     toForward();
   }
 
-  if(S1_VALUE && S2_VALUE && S5_VALUE > 200 && S3_VALUE && S4_VALUE < 100 ){
-    toRight();
-  }
-
-  if(S1_VALUE && S4_VALUE && S5_VALUE > 200 && S3_VALUE && S2_VALUE < 100 ){
+  //If only left sensor detects black line 1-0-1-1-1
+  if((s1v > lineThresh) && (s2v < noLineThresh)  && (s3v > lineThresh) && (s4v > lineThresh) && (s5v > lineThresh)){
     toLeft();
   }
 
-  if(S1_VALUE && S2_VALUE && S3_VALUE && S4_VALUE && S5_VALUE < 200 ){
+  //If only left most sensor detects black line 0-1-1-1-1
+  if((s1v < noLineThresh)  && (s2v > lineThresh) && (s3v > lineThresh) && (s4v > lineThresh) && (s5v > lineThresh)){  
+    performTurn(FORWARD, BACKWARD);
+  }
+
+  //If only right sensor detects black line 1-1-1-0-1
+  if((s1v > lineThresh) && (s2v > lineThresh)  && (s3v > lineThresh) && (s4v < noLineThresh) && (s5v > lineThresh)){ 
+    toRight();
+  }
+
+  //If only right most sensor detects black line 1-1-1-1-0
+  if((s1v > lineThresh) && (s2v > lineThresh)  && (s3v > lineThresh) && (s4v > lineThresh) && (s5v < noLineThresh)){ 
+    performTurn(BACKWARD, FORWARD);
+  } 
+
+  //If middle and right sensor detects black line 1-1-0-0-1
+  if((s1v > lineThresh) && (s2v > lineThresh)  && (s3v < noLineThresh) && (s4v < noLineThresh) && (s5v > lineThresh)){ //
+    toRight();
+  } 
+
+  //If middle and left sensor detects black line 1-0-0-1-1
+  if((s1v > lineThresh) && (s2v < noLineThresh)  && (s3v < noLineThresh) && (s4v > lineThresh) && (s5v > lineThresh)){
+    toLeft();
+  }
+
+  //If middle, left and left most sensor detects black line 0-0-0-1-1
+  if((s1v < noLineThresh) && (s2v < noLineThresh)  && (s3v < noLineThresh) && (s4v > lineThresh) && (s5v > lineThresh)){
+    toLeft();
+  }
+
+  //If middle, right and right most sensor detects black line 1-1-0-0-0
+  if((s1v > lineThresh) && (s2v > lineThresh)  && (s3v < noLineThresh) && (s4v < noLineThresh) && (s5v < noLineThresh)){
+    toRight();
+  } 
+
+  //If all sensors are off black line 1-1-1-1-1
+  if((s1v > lineThresh) && (s2v > lineThresh) && (s3v > lineThresh) && (s4v > lineThresh) && (s5v > lineThresh)){
+    toForward();
+  }
+  
+  //If all sensors are on black line 0-0-0-0-0
+  if((s1v < lineThresh) && (s2v < lineThresh) && (s3v < lineThresh) && (s4v < lineThresh) && (s5v < lineThresh)){ 
     stopMotors();
   }
 }
 
-
 void toForward(){
-  motorE.run(FORWARD);
-  motorE.setSpeed(200);
   motorD.run(FORWARD);
-  motorD.setSpeed(200);
+  motorE.run(FORWARD);
+  motorD.setSpeed(motorSpeed);
+  motorE.setSpeed(motorSpeed);
 }
 
 void toRight(){
- //motorD.run(RELEASE);
- motorD.setSpeed(80);
+  motorD.run(RELEASE);
+  motorE.run(FORWARD);
+  motorE.setSpeed(motorSpeed);
 }
 
 void toLeft(){
-  //motorE.run(RELEASE);
-  motorE.setSpeed(80);
+  motorD.run(FORWARD);
+  motorE.run(RELEASE);
+  motorD.setSpeed(motorSpeed);
 }
 
 void stopMotors(){
-  motorE.run(RELEASE);
   motorD.run(RELEASE);
+  motorE.run(RELEASE);
 }
- 
+
+void performTurn(uint8_t x, uint8_t y) {
+    motorD.run(x);
+    motorE.run(y);
+    motorD.setSpeed(turningMotorSpeed);
+    motorE.setSpeed(turningMotorSpeed);
+}
